b46a3511c70730f328513eb107fe4258f4b2723e
skywalker182 <nast.shiyanova@gmail.com>
2021-07-16 15:12:18 +0300

    обновила методы для тестов

--- lib/my_requests.py
+++ lib/my_requests.py
@@ -30,7 +30,7 @@ class MyRequests():
         if cookies is None:
             cookies: {}
 
-        #Logger.add_request(url, data, headers, cookies, method)
+        Logger.add_request(url, data, headers, cookies, method)
 
         if method == 'GET':
             response = requests.get(url, params=data, headers=headers, cookies=cookies)
@@ -43,5 +43,5 @@ class MyRequests():
         else:
             raise Exception(f"Bad HTTP method '{method} was received")
 
-        #Logger.add_response(response)
+        Logger.add_response(response)
         return response
--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -1,16 +1,17 @@
 import requests
 from lib.BaseCase import BaseCase
 from lib.Assertions import Assertions
+from lib.my_requests import MyRequests
 
-login_url = 'https://playground.learnqa.ru/api/user/login'
-user_url = 'https://playground.learnqa.ru/api/user/'
+login_url = '/api/user/login'
+user_url = '/api/user'
 
 
 class TestUserEdit(BaseCase):
     def test_edit_just_created_user(self):
         # Register
         register_data = self.prepare_registration_data()
-        response1 = requests.post(user_url, data=register_data)
+        response1 = MyRequests.post(user_url, data=register_data)
 
         Assertions.assert_code_status(response1, 200)
         Assertions.assert_json_has_key(response1, "id")
@@ -25,14 +26,14 @@ class TestUserEdit(BaseCase):
             'email': email,
             'password': password
         }
-        response2 = requests.post(url=login_url, data=login_data)
+        response2 = MyRequests(url=login_url, data=login_data)
         auth_sid = self.get_cookie(response2, 'auth_sid')
         token = self.get_header(response2, 'x-csrf-token')
 
         # Edit
         new_name = "changedName"
-        new_url = user_url + str(user_id)
-        response3 = requests.put(
+        new_url = user_url + "/" + str(user_id)
+        response3 = MyRequests.put(
             new_url,
             headers={'x-csrf-token': token},
             cookies={'auth_sid': auth_sid},
@@ -42,7 +43,7 @@ class TestUserEdit(BaseCase):
         Assertions.assert_code_status(response3, 200)
 
         # Get
-        response4 = requests.get(
+        response4 = MyRequests.get(
             new_url,
             headers={'x-csrf-token': token},
             cookies={'auth_sid': auth_sid}
@@ -58,7 +59,7 @@ class TestUserEdit(BaseCase):
     def test_edit_just_created_user_too_incorrect_email(self):
         # Register
         register_data = self.prepare_registration_data()
-        response1 = requests.post(user_url, data=register_data)
+        response1 = MyRequests.post(user_url, data=register_data)
 
         Assertions.assert_code_status(response1, 200)
         Assertions.assert_json_has_key(response1, "id")
@@ -73,14 +74,14 @@ class TestUserEdit(BaseCase):
             'email': email,
             'password': password
         }
-        response2 = requests.post(url=login_url, data=login_data)
+        response2 = MyRequests.post(url=login_url, data=login_data)
         auth_sid = self.get_cookie(response2, 'auth_sid')
         token = self.get_header(response2, 'x-csrf-token')
 
         # Edit
         new_email = "changedemail"
-        new_url = user_url + str(user_id)
-        response3 = requests.put(
+        new_url = user_url + "/" + str(user_id)
+        response3 = MyRequests.put(
             new_url,
             headers={'x-csrf-token': token},
             cookies={'auth_sid': auth_sid},
@@ -93,7 +94,7 @@ class TestUserEdit(BaseCase):
     def test_edit_just_created_user_too_short_name(self):
         # Register
         register_data = self.prepare_registration_data()
-        response1 = requests.post(user_url, data=register_data)
+        response1 = MyRequests.post(user_url, data=register_data)
 
         Assertions.assert_code_status(response1, 200)
         Assertions.assert_json_has_key(response1, "id")
@@ -108,14 +109,14 @@ class TestUserEdit(BaseCase):
             'email': email,
             'password': password
         }
-        response2 = requests.post(url=login_url, data=login_data)
+        response2 = MyRequests.post(url=login_url, data=login_data)
         auth_sid = self.get_cookie(response2, 'auth_sid')
         token = self.get_header(response2, 'x-csrf-token')
 
         # Edit
         new_name = "c"
         new_url = user_url + str(user_id)
-        response3 = requests.put(
+        response3 = MyRequests.put(
             new_url,
             headers={'x-csrf-token': token},
             cookies={'auth_sid': auth_sid},
@@ -128,7 +129,7 @@ class TestUserEdit(BaseCase):
     def test_edit_just_created_user_without_auth(self):
         # Register
         register_data = self.prepare_registration_data()
-        response1 = requests.post(user_url, data=register_data)
+        response1 = MyRequests.post(user_url, data=register_data)
 
         Assertions.assert_code_status(response1, 200)
         Assertions.assert_json_has_key(response1, "id")
@@ -141,11 +142,10 @@ class TestUserEdit(BaseCase):
         # Edit
         new_name = "changedName"
         new_url = user_url + str(user_id)
-        response3 = requests.put(
+        response3 = MyRequests.put(
             new_url,
             data={'firstName': new_name}
         )
-        print(response3.text)
 
         Assertions.assert_code_status(response3, 400)
         Assertions.assert_response_text(response3, "Auth token not supplied")
@@ -153,7 +153,7 @@ class TestUserEdit(BaseCase):
     def test_edit_just_created_user_with_wrong_token(self):
         # Register #1
         register_data1 = self.prepare_registration_data()
-        response1 = requests.post(user_url, data=register_data1)
+        response1 = MyRequests.post(user_url, data=register_data1)
 
         Assertions.assert_code_status(response1, 200)
         Assertions.assert_json_has_key(response1, "id")
@@ -162,11 +162,10 @@ class TestUserEdit(BaseCase):
 
         # Register #2
         register_data2 = self.prepare_registration_data()
-        response1_2 = requests.post(user_url, data=register_data2)
+        response1_2 = MyRequests.post(user_url, data=register_data2)
 
         Assertions.assert_code_status(response1_2, 200)
         Assertions.assert_json_has_key(response1_2, "id")
-        print(response1_2.text)
 
         email = register_data2['email']
         password = register_data2['password']
@@ -176,14 +175,14 @@ class TestUserEdit(BaseCase):
             'email': email,
             'password': password
         }
-        response2 = requests.post(url=login_url, data=login_data)
+        response2 = MyRequests.post(url=login_url, data=login_data)
         auth_sid = self.get_cookie(response2, 'auth_sid')
         token = self.get_header(response2, 'x-csrf-token')
 
         # Edit
         new_name = "changedName"
         new_url = user_url + str(user_id)
-        response3 = requests.put(
+        response3 = MyRequests.put(
             new_url,
             headers={'x-csrf-token': token},
             cookies={'auth_sid': auth_sid},
@@ -191,16 +190,13 @@ class TestUserEdit(BaseCase):
         )
 
         Assertions.assert_code_status(response3, 200)  # а должно ли быть 200
-        print("text after edit ", response3.text)
 
         # Get
-        response4 = requests.get(
+        response4 = MyRequests.get(
             new_url,
             headers={'x-csrf-token': token},
             cookies={'auth_sid': auth_sid}
         )
-        print(response4.text)
-
         Assertions.assert_json_value_by_name(
             response4,
             "username",
