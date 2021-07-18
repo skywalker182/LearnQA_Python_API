6bffdbcc98a7608be1a9d3590ccbeddbd56a04ea
skywalker182 <nast.shiyanova@gmail.com>
2021-07-18 14:08:48 +0300

    allure теги

--- tests/test_user_delete.py
+++ tests/test_user_delete.py
@@ -1,3 +1,4 @@
+import allure
 import requests
 from lib.Assertions import Assertions
 from lib.BaseCase import BaseCase
@@ -6,7 +7,29 @@ from lib.my_requests import MyRequests
 login_url = '/user/login'
 url_for_operations_with_user = '/user'
 
+TEST_CASE_LINK = 'https://software-testing.ru/lms/mod/assign/view.php?id=207365'
+
+
 class TestUserDelete(BaseCase):
+    @allure.epic('Удаление пользователя')
+    @allure.feature('Удаление пользователя авторизованным юзером')
+    @allure.story('попытку удалить пользователя по ID = 2')
+    @allure.testcase(TEST_CASE_LINK, 'Тесты на DELETE')
+    @allure.severity(allure.severity_level.NORMAL)
+    @allure.description_html("""
+    <h2>Данные для авторизации: </h2>
+    <p>попытку удалить пользователя по ID = 2</p>
+    <table style="width:100%">
+      <tr>
+        <th>email</th>
+        <th>password</th>
+      </tr>
+      <tr align="center">
+        <td>vinkotov@example.com</td>
+        <td>1234</td>
+      </tr>
+    </table>
+    """)
     def test_delete_user_with_id_lower_five(self):
         # Login
         data = {
@@ -27,6 +50,12 @@ class TestUserDelete(BaseCase):
         Assertions.assert_code_status(response2, 400)
         Assertions.assert_response_text(response2, 'Please, do not delete test users with ID 1, 2, 3, 4 or 5.')
 
+    @allure.testcase(TEST_CASE_LINK, 'Тесты на DELETE')
+    @allure.epic('Удаление пользователя')
+    @allure.feature('Удаление пользователя авторизованным юзером')
+    @allure.story('удаление пользоваля только что созданного пользователя с тем же id, что и залогиненный юзер')
+    @allure.description('удаление пользоваля только что созданного пользователя с тем же id, что и залогиненный юзер')
+    @allure.severity(allure.severity_level.TRIVIAL)
     def test_delete_just_created_user_with_same_id(self):
         # Register
         register_data = self.prepare_registration_data()
@@ -69,6 +98,12 @@ class TestUserDelete(BaseCase):
         # проверяем, что удалился и не найден
         Assertions.assert_code_status(response4, 404)
 
+    @allure.epic('Удаление пользователя')
+    @allure.feature('Удаление пользователя авторизованным юзером')
+    @allure.story('удаление пользоваля только что созданного пользователя с id, отличающимся от залогиненого юзера')
+    @allure.testcase(TEST_CASE_LINK, 'Тесты на DELETE')
+    @allure.description('удаление пользоваля только что созданного пользователя из-под другого юзера')
+    @allure.severity(allure.severity_level.CRITICAL)
     def test_delete_just_created_user_with_different_id(self):
         # Register #1
         register_data1 = self.prepare_registration_data()
@@ -117,4 +152,3 @@ class TestUserDelete(BaseCase):
         )
         # проверка, что не удалился
         Assertions.assert_code_status(response4, 200)
-
